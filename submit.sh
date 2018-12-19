8113b7a7b4070cf258aa106b0487c6e3b2ae9478
Jessica Gray <jg1994@uncw.edu>
2018-12-19 12:48:08 -0500

    Submit a Job

---
+++ submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J Practice         # Job name
+#SBATCH -e myjob.e%j       # Name of stderr error file
+#SBATCH -p skx-normal          # Queue (partition) name
+#SBATCH -N 1               # Total # of nodes (must be 1 for serial)
+#SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
+#SBATCH -t 00:01:00        # Run time (hh:mm:ss)
+#SBATCH --mail-user=jg1994@uncw.edu
+#SBATCH --mail-type=all    # Send email at begin and end of job
+# Other commands must follow all #SBATCH directives...
+mkdir output
+Rscript --vanilla --verbose ./Practice.R > ./output.Rout
